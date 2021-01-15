9806687c79cd7f54e506a919f36e4c7de7e287a7
rober <carderober@gmail.com>
2021-01-15 18:39:05 -0300

    Tabla admin, estilos, seguridad form

--- cfa-plugin-wp.php
+++ cfa-plugin-wp.php
@@ -74,7 +74,7 @@ function CFA_Pluguin_Form()
         $mensaje = sanitize_text_field( $_POST['mensaje'] );
         $created_at = date('Y-m-d H:i:s'); 
 
-        // Insertado datos.
+        // Si el perfil es operador y completo los campos sector y compania.
         if ($_POST['perfil'] == 'Operador de telecomunicaciones' 
             AND $_POST['sector'] != '' 
             AND $_POST['compania'] != ''
@@ -83,6 +83,7 @@ function CFA_Pluguin_Form()
             $compania = sanitize_text_field( $_POST['compania'] );
         }
 
+        // Insertanado los datos saneados.
         $wpdb->insert($tabla_mensajes, [
             'nombre' => $nombre,
             'apellido' => $apellido,
@@ -99,11 +100,15 @@ function CFA_Pluguin_Form()
         ]);
     }
 
+    // Carga esta hoja de estilo para poner más bonito el formulario
+    wp_enqueue_style('css_form_alpha', plugins_url('includes/css/style.css', __FILE__));
+
     // Form que se insertara con el shortcode
     ob_start();
     ?>
         <!-- Contact Form Alpha -->
         <form method="POST" action="<?php get_the_permalink(); ?>" class="form-alpha">
+        <?php wp_nonce_field('contact_form', 'contact_nonce') ?>
             <div class="form-input">
                 <label for="nombre">Nombre</label>
                 <input type="text" name="nombre" id="nombre" required>
@@ -221,5 +226,73 @@ function CFA_Pluguin_Form()
         <!-- End Scripts -->
     <?php
     return ob_get_clean();
-};
+}
+
+add_action( 'admin_menu', 'CFA_Mensajes_Menu');
+
+function CFA_Mensajes_Menu()
+{
+    add_menu_page( 'Mensajes Recibidos', 'Mensajes', 'manage_options', 'cfa_mensajes_menu', 'CFA_Mensajes_Admin', 'dashicons-feedback', 75 );
+}
+
+function CFA_Mensajes_Admin()
+{
+    global $wpdb;
+    $tabla_mensajes = $wpdb->prefix . 'mensaje';
+    $mensajes = $wpdb->get_results("SELECT * FROM $tabla_mensajes");
+    ob_start();
+    ?>
+    <div class="wrap"><h1>Lista de mensajes</h1>
+        <table class="wp-list-table widefat fixed striped">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Correo</th>
+                    <th>Celular</th>
+                    <th>Provincia</th
+                    ><th>Localidad</th>
+                    <th>Perfil</th>
+                    <th>Sector</th>
+                    <th>Compania</th>
+                    <th>Servicio</th>
+                    <th>Mensaje</th>
+                    <th>Recibido</th>
+                </tr>
+            </thead>
+            <tbody id="the-list">
+            <?php  
+                foreach ($mensajes as $mensaje) {
+                    $nombre = esc_textarea($mensaje->nombre);
+                    $apellido = esc_textarea($mensaje->apellido);
+                    $correo = esc_textarea($mensaje->correo);
+                    $celular = esc_textarea($mensaje->celular);
+                    $provincia = esc_textarea($mensaje->provincia);
+                    $localidad = esc_textarea($mensaje->localidad);
+                    $perfil = esc_textarea($mensaje->perfil);
+                    $sector = esc_textarea($mensaje->sector);
+                    $compania = esc_textarea($mensaje->compania);
+                    $servicio = esc_textarea($mensaje->servicio);
+                    $msj = esc_textarea($mensaje->mensaje);
+                    $recibido = esc_textarea($mensaje->created_at);
+
+                    echo "<tr><td>$mensaje->nombre</td>";
+                    echo "<td>$mensaje->apellido</td>";
+                    echo "<td>$mensaje->correo</td>";
+                    echo "<td>$mensaje->celular</td>";
+                    echo "<td>$mensaje->provincia</td>";
+                    echo "<td>$mensaje->localidad</td>";
+                    echo "<td>$mensaje->perfil</td>";
+                    echo "<td>$mensaje->sector</td>";
+                    echo "<td>$mensaje->compania</td>";
+                    echo "<td>$mensaje->servicio</td>";
+                    echo "<td>$mensaje->mensaje</td>";
+                    echo "<td>$mensaje->created_at</td></tr>";
+                }
+            ?>
+        </tbody></table></div>
+    <?php
+    echo ob_get_clean();
+    
+}
 
